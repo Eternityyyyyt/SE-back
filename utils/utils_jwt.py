7e1649ea801b60570234a4636c491e0e514c5779
Eternityyyyyt <stoneziheng@126.com>
2024-03-27 09:02:35 +0800

    Fix: adding debugging messages,changing salt to fixed value(#1)

--- utils/utils_jwt.py
+++ utils/utils_jwt.py
@@ -8,7 +8,8 @@ from typing import Optional
 
 # c.f. https://thuse-course.github.io/course-index/basic/jwt/#jwt
 # !Important! Change this to your own salt, better randomly generated!"
-SALT = ("KawaiiNana" + datetime.datetime.now().strftime("%Y%m%d%H%M")).encode("utf-8")
+#SALT = ("KawaiiNana" + datetime.datetime.now().strftime("%Y%m%d%H%M")).encode("utf-8")
+SALT = ("KawaiiNana").encode("utf-8")
 EXPIRE_IN_SECONDS = 60 * 60 * 24 * 1  # 1 day
 ALT_CHARS = "-_".encode("utf-8")
 
