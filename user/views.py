c9f5366ee5f89aadb4bd3ce78161c6838cf6ef25
zhc <zhang-hc22@mails.tsinghua.edu.cn>
2024-04-02 11:39:23 +0800

    fix: enable to send friend request again when last request denyed or friendship deleted(#8)

--- user/views.py
+++ user/views.py
@@ -165,8 +165,11 @@ def send_friend_request(req: HttpRequest, receiverName:any) :
                 if friends.filter(userName = receiverName):
                     return request_failed(5, "He/She is already your friend", 400)
                 else:
-                    if FriendRequest.objects.filter(sender=sender, receiver=receiver).exists():
-                        return request_failed(3,"Friend request already exists", 400)
+                    friendRequestToMe = FriendRequest.objects.filter(sender=sender, receiver=receiver).last()
+                    if friendRequestToMe:
+                        if friendRequestToMe.status == 0:
+                            return request_failed(3,"Friend request already exists", 400)
+                        # else: continue
                     if FriendRequest.objects.filter(sender=receiver, receiver=sender).exists():
                         return request_failed(6,"He/she has already sent a friend request to you, please handle it first", 400)
                     else:
