5c8c879e393e4886e651bb2ca0f6c97853d69242
Eternityyyyyt <stoneziheng@126.com>
2024-04-15 14:45:32 +0800

    add unitest for chat app(#11)

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,198 @@
 from django.test import TestCase
+from user.models import User,FriendRequest
+from chat.models import Chat,Message,GroupNotice
+from typing import Optional
+import datetime
+import hashlib
+import hmac
+import time
+import json
+import base64
+import random
+
+from utils.utils_jwt import EXPIRE_IN_SECONDS, SALT, b64url_encode
 
 # Create your tests here.
+class UserTest(TestCase):
+    def setUp(self) -> None:
+        test_user_name = "testuser"
+        testuser = User.objects.create(userName="testuser",password = "123456", phoneNumber ="12345678901", email = "qwe@qwe.qwe",nickname = "测试" )
+        testuser2 = User.objects.create(userName="testuser2",password = "123456", phoneNumber ="12345678901", email = "qwe@qwe.qwe",nickname = "测试2" )
+        testuser3 = User.objects.create(userName="testuser3",password = "123456", phoneNumber ="12345678901", email = "qwe@qwe.qwe",nickname = "测试3" )
+        testuser.friends.add(testuser2)
+        chat = Chat.objects.create(chatName = "testuser and testuser2")
+        chat.memberList.add(testuser2)
+        chat.memberList.add(testuser)
+        
+        return super().setUp()
+    # ! Utility functions
+    def generate_jwt_token(self, payload: dict, salt: str):
+        # * header
+        header = {
+            "alg": "HS256",
+            "typ": "JWT"
+        }
+        # dump to str. remove `\n` and space after `:`
+        header_str = json.dumps(header, separators=(",", ":"))
+        # use base64url to encode, instead of base64
+        header_b64 = b64url_encode(header_str)
+        
+        # * payload
+        payload_str = json.dumps(payload, separators=(",", ":"))
+        payload_b64 = b64url_encode(payload_str)
+        
+        # * signature
+        signature_str = header_b64 + "." + payload_b64
+        signature = hmac.new(salt, signature_str.encode("utf-8"), digestmod=hashlib.sha256).digest()
+        signature_b64 = b64url_encode(signature)
+        
+        return header_b64 + "." + payload_b64 + "." + signature_b64
+
+    def generate_header(self, username: str, payload: dict = {}, salt: str = SALT):
+        if len(payload) == 0:
+            payload = {
+                "iat": int(time.time()),
+                "exp": int(time.time()) + EXPIRE_IN_SECONDS,
+                "data": {
+                    "userName": username
+                }
+            }
+        return {
+            "HTTP_AUTHORIZATION": self.generate_jwt_token(payload, salt)
+        }
+    
+    #Test section
+    def test_create_private_chat_success(self):
+        creater = "testuser"
+        member = "testuser3"
+        userCreater = User.objects.filter(userName=creater).first()
+        userMember = User.objects.filter(userName=member).first()
+        userCreater.friends.add(userMember)
+        headers = self.generate_header(username="testuser")
+        data = {
+            "createrName": creater,
+            "memberName": member
+        }
+        res = self.client.post("/chat/createPrivate",data=data, content_type="application/json", **headers)
+        self.assertEqual(res.status_code , 200)
+        self.assertEqual(res.json()['code'] , 0)
+        self.assertEqual(res.json()['data']['alreadyCreated'],False)
+        self.assertEqual(res.json()['data']['chat_id'],2)
+    def test_create_private_chat_not_friend(self):
+        creater = "testuser"
+        member = "testuser3"
+        headers = self.generate_header(username="testuser")
+        data = {
+            "createrName": creater,
+            "memberName": member
+        }
+        res = self.client.post("/chat/createPrivate",data=data, content_type="application/json", **headers)
+        self.assertEqual(res.status_code , 403)
+        self.assertEqual(res.json()['code'] , 3)
+    def test_create_private_chat_exist_success(self):     
+        creater = "testuser"
+        member = "testuser2"
+        headers = self.generate_header(username="testuser")
+        data = {
+            "createrName": creater,
+            "memberName": member
+        }
+        res = self.client.post("/chat/createPrivate",data=data, content_type="application/json", **headers)
+        self.assertEqual(res.status_code , 200)
+        self.assertEqual(res.json()['code'] , 0)
+        self.assertEqual(res.json()['data']['alreadyCreated'],True)
+    def test_create_private_chat_with_wrong_jwt(self):
+        creater = "testuser"
+        member = "testuser2"
+        userCreater = User.objects.filter(userName=creater).first()
+        userMember = User.objects.filter(userName=member).first()
+        userCreater.friends.add(userMember)
+        headers = self.generate_header(username="nottestuser")
+        data = {
+            "createrName": creater,
+            "memberName": member
+        }
+        res = self.client.post("/chat/createPrivate",data=data, content_type="application/json", **headers)
+        self.assertEqual(res.status_code , 401)
+        self.assertEqual(res.json()['code'] , 2)
+    def test_create_private_chat_creater_not_exist(self):
+        creater = "notexistuser"
+        member = "testuser2"
+        headers = self.generate_header(username="notexist")
+        data = {
+            "createrName": creater,
+            "memberName": member
+        }
+        res = self.client.post("/chat/createPrivate",data=data, content_type="application/json", **headers)
+        self.assertEqual(res.status_code , 404)
+        self.assertEqual(res.json()['code'] , 1)
+
+    #Test posting message
+    def test_post_message_in_private_chat_success(self):
+        member1 = "testuser"
+        member2 = "testuser2"
+        headers = self.generate_header(username=member1)
+        data = {
+            "userName": member1,
+            "chat_id": 1,
+            "content": "Hello, I am your father",
+            "replying": 0
+        }
+        res = self.client.post("/chat/message",data=data, content_type="application/json", **headers)
+        self.assertEqual(res.status_code , 200)
+        self.assertEqual(res.json()['code'] , 0)
+
+    def test_post_message_in_private_chat_not_exist(self):
+        member1 = "testuser"
+        member2 = "testuser2"
+        headers = self.generate_header(username=member1)
+        data = {
+            "userName": member1,
+            "chat_id": 2,
+            "content": "Hello, I am your father",
+            "replying": 0
+        }
+        res = self.client.post("/chat/message",data=data, content_type="application/json", **headers)
+        self.assertEqual(res.status_code , 404)
+        self.assertEqual(res.json()['code'] , 1)
+    def test_post_message_in_private_chat_user_not_in_chat(self):
+        member1 = "testuser3"
+        headers = self.generate_header(username=member1)
+        data = {
+            "userName": member1,
+            "chat_id": 1,
+            "content": "Hello, I am your father",
+            "replying": 0
+        }
+        res = self.client.post("/chat/message",data=data, content_type="application/json", **headers)
+        self.assertEqual(res.status_code , 404)
+        self.assertEqual(res.json()['code'] , 3)
+
+    def test_post_message_in_private_chat_wrong_jwt(self):
+        member1 = "testuser"
+        member2 = "testuser2"
+        headers = self.generate_header(username="nottestuser")
+        data = {
+            "userName": member1,
+            "chat_id": 1,
+            "content": "Hello, I am your father",
+            "replying": 0
+        }
+        res = self.client.post("/chat/message",data=data, content_type="application/json", **headers)
+        self.assertEqual(res.status_code , 401)
+        self.assertEqual(res.json()['code'] , 2)
+
+   # def test_get_message_in_private_chat_success(self):
+      #  testuser = User.objects.filter(userName="testuser").first()
+      #  chat = Chat.objects.filter(chat_id = 1).first()
+      #  Message.objects.create(belongToChat=chat, content="a message sent by testuser",sender=testuser)
+      #  headers = self.generate_header(username="testuser")
+      #  data = {
+      #      "userName": "testuser",
+      #      "chat_id": 1,
+      #      "after": 0,
+      #      "limit": 100
+      #  }
+      #  res = self.client.get("/chat/message",data=data, content_type="application/json", **headers)
+      #  self.assertEqual(res.status_code , 200)
+      #  self.assertEqual(res.json()['code'] , 0)
